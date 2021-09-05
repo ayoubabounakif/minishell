89a4375b7fc3f7e1a1c44763df7c544cb117912e
Khalil Hafni <khafni@e2r3p3.1337.ma>
2021-09-05 18:52:05 +0100

    more robust syntax checking 👮

--- parser/syntax_checking.c
+++ parser/syntax_checking.c
@@ -6,7 +6,7 @@
 /*   By: khafni <khafni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/12 21:19:42 by khafni            #+#    #+#             */
-/*   Updated: 2021/09/05 16:38:57 by khafni           ###   ########.fr       */
+/*   Updated: 2021/09/05 18:50:55 by khafni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,43 +39,72 @@ void		syntax_set_error(t_syx_check sx, char *err_message)
 		sx->error_message = ft_strdup(err_message);
 	}
 }
-
-
-/* int		check_pipes_syntax_at_first(char *parsing_line)
+/* int		go_to_next_pipe(char *parsing_line, char *mask)
 {
-	int	i;
-	int	is_pipe;
+
+}
+ */
+
+void	check_if_between_pipes_is_empty(char *parsing_line, char *mask, t_syx_check syx)
+{
+	int i;
+	int is_only_spaces;
+
+	i = 0;
+	is_only_spaces = 1;	
+	i = ft_strlen(mask) - 1;
+	while (i >= 0 && mask[i] != '|')
+	{
+		if (mask[i] != 'W')	
+			is_only_spaces = 0;
+		i--;
+	}
+	if (is_only_spaces)
+		syntax_set_error(syx, "error aroudn the pipe");
+}
+
+void	check_if_after_last_pipe(char *parsing_line, char *mask, t_syx_check syx)
+{
+	int i;
+	int is_pipe;
 
 	i = 0;
 	is_pipe = 0;
-	if (parsing_line[0] == '|')
-		is_pipe = 1;	
-	while (parsing_line[i])
+	while (mask[i])
 	{
-		if ()	
-		i++;
+		if ((is_pipe) && (mask[i] != '|') && mask[i] != 'W')
+			is_pipe = 0;
+		if (is_pipe && mask[i] == '|')
+			syntax_set_error(syx, "error around the pipe");
+		else if (!is_pipe && mask[i] == '|') 
+			is_pipe = 1;
+			i++;
 	}
-} */
-
+}
 void    check_pipes_n_semiclns(char *parsing_line, t_syx_check syx)
 {
 	int i;
 	int is_pipe;
+	char	*mask;
 	
+	mask = get_mask(parsing_line);
 	i = 0;
 	is_pipe = 0;
-	if (parsing_line[0] == '|')
+	check_if_between_pipes_is_empty(parsing_line, mask, syx);
+	check_if_after_last_pipe(parsing_line, mask, syx);
+	if (mask[0] == '|')
 		syntax_set_error(syx, "parse error around the pipe");
-	while (parsing_line[i])
+	while (mask[i])
 	{
-		if ((is_pipe) && (parsing_line[i] != '|'))
+		if ((is_pipe) && (mask[i] != '|'))
 			is_pipe = 0;		
-		if (is_pipe && parsing_line[i] == '|') 
-			syntax_set_error(syx, "error aroudn the pipe"); 	 
-		else if (!is_pipe && parsing_line[i] == '|') 
+		if (is_pipe && mask[i] == '|') 
+			syntax_set_error(syx, "error around the pipe"); 	 
+		else if (!is_pipe && mask[i] == '|') 
 			is_pipe = 1; 	
-		if (ft_strlen(parsing_line) >= 1 && (parsing_line[ft_strlen(parsing_line) - 1] == '|'))
+		if (ft_strlen(parsing_line) >= 1 && (mask[ft_strlen(parsing_line) - 1] == '|'))
 			syntax_set_error(syx, "error around the pipe or semicolon");
 		i++;
 	}
+	free(mask);
 }
